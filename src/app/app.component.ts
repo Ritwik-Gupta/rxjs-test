7ff28c489bcaa0b7ce3fade6f89a369a0f7beb55
Ritwik <ritwik94348gupta@gmail.com>
2023-10-12 23:51:40 +0530

    Observables basic examples, from arrays and promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-test';
+
+  testArray = [
+    {title: "test title1", description: "test description 1"},
+    {title: "test title2", description: "test description 2"},
+    {title: "test title3", description: "test description 3"}
+  ]
+
+  promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Resolving the promise...done!')
+    }, 3000)
+  })
+
+  ArrayObservable$ = from(this.testArray);
+  PromiseObservable$ = from(this.promise);
+
+  constructor() {
+
+    this.ArrayObservable$.subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log("Done!")
+    });
+
+    this.PromiseObservable$.subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log("Ok done")
+    })
+  }
 }
