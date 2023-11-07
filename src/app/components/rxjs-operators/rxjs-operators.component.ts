c226a5e66f4653ad9334503b72e6b9f371bb95f6
Ritwik <ritwik94348gupta@gmail.com>
2023-11-08 02:11:40 +0530

    RxJS Operators and Pipes basics

---
+++ src/app/components/rxjs-operators/rxjs-operators.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { filter, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs-operators',
+  templateUrl: './rxjs-operators.component.html',
+  styleUrls: ['./rxjs-operators.component.css']
+})
+export class RxjsOperatorsComponent implements OnInit{
+
+  ngOnInit(): void {
+    let testObservable = interval(100);
+
+    // testObservable.subscribe(data => {
+    //   if(data%2 === 0) {
+    //     console.log(data);
+    //   }
+    // });
+
+    testObservable.pipe(filter(data => data%2==0), filter(data => data%4==0)).subscribe(data => console.log(data));
+
+    //Notes
+    //1. Operators
+    //2. Pipes
+
+    //Operators - returns an observable(eg. of, fromEvent, filter)
+    //Pipes - chaining of observables is done in pipes, output of 1st pipex param is passed to the next param
+``
+    //Category of Operators
+    //1. Transformation
+    //2. Filtering
+    //3. Utility
+    //4. Conditional
+    //5. Aggregate
+    //6. Multicast
+  }
+
+}
